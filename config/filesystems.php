7be018d52df2036cf47a2ef60f787da1641d8465
Socheat Sok <alex@socheat.net>
2021-03-20 21:50:45 +0700

    Revert "feat(config): add 'minio' disks and MINIO_ENDPOINT env"
    This reverts commit 47928f96bddb0bef18680f9407c91b7a0c99ef54.

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,16 +52,6 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
-        'minio' => [
-            'driver' => 's3',
-            'endpoint' => env('MINIO_ENDPOINT', 'http://127.0.0.1:9000'),
-            'key' => env('AWS_KEY'),
-            'secret' => env('AWS_SECRET'),
-            'region' => env('AWS_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'use_path_style_endpoint' => true,
-        ],
-
     ],
 
     /*
